624aa6d82d2da7e13756e8161794d61f9cfcd748
Illia <zaqwery@gmail.com>
2010-04-03 14:29:50 +0300

    plugins, scaffolds added, unused .js removed

---
+++ db/migrate/20100403110840_create_books.rb
@@ -0,0 +1,16 @@
+class CreateBooks < ActiveRecord::Migration
+  def self.up
+    create_table :books do |t|
+      t.string :name
+      t.string :author
+      t.text :description
+      t.integer :year
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :books
+  end
+end
